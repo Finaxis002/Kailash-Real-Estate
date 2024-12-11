391ee394fe6953d2b27aa0991cfdb0b8db08b90a
finaxis002 <finaxis.ai@gmail.com>
2024-12-11 14:34:13 +0530

    about us page

--- jasvscript/script.js
+++ jasvscript/script.js
@@ -1,19 +1,6 @@
-function includeHTML() {
-    const elements = document.querySelectorAll("[data-include]");
-    elements.forEach(async (element) => {
-        const file = element.getAttribute("data-include");
-        if (file) {
-            try {
-                const response = await fetch(file);
-                if (!response.ok) throw new Error(`Could not load ${file}`);
-                const content = await response.text();
-                element.innerHTML = content;
-            } catch (error) {
-                element.innerHTML = `Error loading ${file}`;
-            }
-        }
-    });
-}
-
-// Run the includeHTML function after DOM is loaded
-document.addEventListener("DOMContentLoaded", includeHTML);
+$(document).ready(function () {
+    $('#head_content').load('assets/header.html');
+    $('#foot_content').load('assets/footer.html');
+    
+    console.log("jsquery is working")
+});
